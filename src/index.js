aaf81c9cc99fafb0e3cd1c6de62f549a1a1b5b63
MaksimTsvik <mkdemix@tut.by>
2021-01-31 18:36:39 +0300

    Final commit. Tests completed.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
+  // Create an empty array for result
   let arr = [];
-
+  // Check for correct input in 'matrix'
   if (matrix === undefined || !matrix.length) {
     return arr;
   }
-
+  /*
+   Calculate one array using reduce() of 'matrix' array with ispection of next element index
+   If next index %2 != 0 -> we need to reverse this element (next array) 
+   concat each element to result array
+  */
   arr = matrix.reduce(function (resultArr, nextElem, index) {
 
-    if (index % 2 != 0) {
+    if (index % 2) {
       return resultArr.concat(nextElem.reverse());
     } else {
       return resultArr.concat(nextElem);
